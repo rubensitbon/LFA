1f2d008cc8c29f92cd4e2cb6ce0750dd3b712e3b
Ruben Sitbon <ruben.sitbon@outlook.com>
2018-11-04 12:31:08 +0100

    Add Google Auth

--- lfafront/src/pages/Home/Home.js
+++ lfafront/src/pages/Home/Home.js
@@ -1,47 +1,29 @@
 // @flow
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
-import type { RouterHistory } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+import type { RouterHistory } from "react-router-dom";
+import GoogleLogin from "react-google-login";
+import { GoogleLogout } from "react-google-login";
 
-import StyledIntro from './Home.style';
+import StyledIntro from "./Home.style";
 
-const navigateTo = (history: RouterHistory): (() => void) => (): void => {
-  history.push('/avatar');
+const responseGoogle = response => {
+  console.log(response);
 };
 
 type Props = {
-  history: RouterHistory,
+  history: RouterHistory
 };
 
 const Home = ({ history }: Props) => (
   <React.Fragment>
-    <StyledIntro>
-      <p>
-        <FormattedMessage id="home.get-started" defaultMessage="To get started, edit" />
-        <code className="intro-code">src/App.js</code>
-        <FormattedMessage id="home.save-to-reload" />
-      </p>
-      <p>
-        <FormattedMessage id="home.create-route" />
-      </p>
-      <p>
-        <FormattedMessage id="home.generate-component" />
-        <code className="intro-code">yarn generate</code>
-      </p>
-      <p>
-        <FormattedMessage id="home.generate-module" />
-      </p>
-      <p>
-        <FormattedMessage id="home.readme" />
-      </p>
-      <button onClick={navigateTo(history)}>
-        <FormattedMessage id="home.click-me" />
-      </button>
-      <Link to="/avatar" href="/avatar">
-        <FormattedMessage id="home.use-a-link" />
-      </Link>
-    </StyledIntro>
+    <GoogleLogin
+      clientId="1033550430707-a9kr1ulnk0hj8v93teiu0ab8q4klfb6o.apps.googleusercontent.com"
+      buttonText="Login with Google"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+    />
   </React.Fragment>
 );
 
